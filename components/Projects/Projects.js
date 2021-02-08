d090be4501c9f89511ec790712e2d39dbb45dfdd
Armand Sallé <armand.salle@gmail.com>
2021-02-08 15:52:06 +0100

    Fix: images quality

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,94 +1,141 @@
-import React, {useState} from 'react'
-import Link from "next/link";
+import React, { useState } from "react"
+import Link from "next/link"
 
-import {Button, Header, Heading, IsoTope, IsoTopeItem, IsoTopMenu, ImageLink, Sticky, ImageGridImage, ImageGrid, ImageGridItem, List, ListItem, LinkItem, Seperator, Sup, Section} from './Projects.styled'
-
+import {
+  Button,
+  Header,
+  Heading,
+  IsoTope,
+  IsoTopeItem,
+  IsoTopMenu,
+  ImageLink,
+  Sticky,
+  ImageGridImage,
+  ImageGrid,
+  ImageGridItem,
+  List,
+  ListItem,
+  LinkItem,
+  Seperator,
+  Sup,
+  Section,
+} from "./Projects.styled"
 
 const Projects = ({ projects, title, active }) => {
+  const [hover, setHover] = useState(false)
 
-    const [hover, setHover] = useState(false);
+  return (
+    <Section>
+      <Sticky>
+        <Header onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+          {title && <Heading>{title}</Heading>}
+          <Seperator />
+          {hover && (
+            <IsoTope className="hover">
+              <IsoTopeItem className={active === "/work" ? "is-active all" : "all"}>
+                <Link href="/work">
+                  <Button>
+                    <span className="sr-only">View </span> All
+                  </Button>
+                </Link>
+              </IsoTopeItem>
 
-    return (
-        <Section>
-            <Sticky>
-                <Header onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                    {title && <Heading>{title}</Heading>}
-                    <Seperator />
-                    {hover && (
-                        <IsoTope className="hover">
-                            <IsoTopeItem className={active === '/work' ? 'is-active all' : 'all'}>
-                                <Link href="/work">
-                                    <Button><span className="sr-only">View </span> All</Button>
-                                </Link>
-                            </IsoTopeItem>
+              <IsoTopeItem className={active === "/work/branding" ? "is-active branding" : "branding"}>
+                <Link href="/work/branding">
+                  <Button>
+                    <span className="sr-only">View </span> Branding
+                  </Button>
+                </Link>
+              </IsoTopeItem>
 
-                            <IsoTopeItem className={active === '/work/branding' ? 'is-active branding' : 'branding'}>
-                                <Link href="/work/branding">
-                                    <Button><span className="sr-only">View </span> Branding</Button>
-                                </Link>
-                            </IsoTopeItem>
+              <IsoTopeItem className={active === "/work/ux-design" ? "is-active ux-design" : "ux-design"}>
+                <Link href="/work/ux-design">
+                  <Button>
+                    <span className="sr-only">View </span> UX Design
+                  </Button>
+                </Link>
+              </IsoTopeItem>
+            </IsoTope>
+          )}
+          {!hover && (
+            <IsoTope>
+              {active === "/work" ? (
+                <IsoTopeItem className={active === "/work" ? "is-active all" : "all"}>
+                  <Link href="/work">
+                    <Button>
+                      <span className="sr-only">View </span> All
+                    </Button>
+                  </Link>
+                </IsoTopeItem>
+              ) : null}
 
-                            <IsoTopeItem className={active === '/work/ux-design' ? 'is-active ux-design' : 'ux-design'}>
-                                <Link href="/work/ux-design">
-                                    <Button><span className="sr-only">View </span> UX Design</Button>
-                                </Link>
-                            </IsoTopeItem>
-                        </IsoTope>
-                    )}
-                    {!hover &&
-                    <IsoTope>
-                        {active === '/work' ? (
-                            <IsoTopeItem className={active === '/work' ? 'is-active all' : 'all'}>
-                                <Link href="/work">
-                                    <Button><span className="sr-only">View </span> All</Button>
-                                </Link>
-                            </IsoTopeItem>
-                        ) : null}
+              {active === "/work/branding" ? (
+                <IsoTopeItem className={active === "/work/branding" ? "is-active branding" : "branding"}>
+                  <Link href="/work/branding">
+                    <Button>
+                      <span className="sr-only">View</span> Branding
+                    </Button>
+                  </Link>
+                </IsoTopeItem>
+              ) : null}
 
-                        {active === '/work/branding' ? (
-                            <IsoTopeItem className={active === '/work/branding' ? 'is-active branding' : 'branding'}>
-                                <Link href="/work/branding">
-                                    <Button><span className="sr-only">View</span> Branding</Button>
-                                </Link>
-                            </IsoTopeItem>
-                        ) : null }
+              {active === "/work/ux-design" ? (
+                <IsoTopeItem className={active === "/work/ux-design" ? "is-active ux-design" : "ux-design"}>
+                  <Link href="/work/ux-design">
+                    <Button>
+                      <span className="sr-only">View</span> UX Design
+                    </Button>
+                  </Link>
+                </IsoTopeItem>
+              ) : null}
+            </IsoTope>
+          )}
+        </Header>
+        <List>
+          {projects &&
+            projects.map((project) => {
+              const { id, slug, title } = project
+              const increment = `0${id}`
 
-                        {active === '/work/ux-design' ? (
-                            <IsoTopeItem className={active === '/work/ux-design' ? 'is-active ux-design' : 'ux-design'}>
-                                <Link href="/work/ux-design">
-                                    <Button><span className="sr-only">View</span> UX Design</Button>
-                                </Link>
-                            </IsoTopeItem>
-                        ) : null }
-                    </IsoTope>
-                    }
-                </Header>
-                <List>
-                    {projects && projects.map( project => {
-                        const { id, slug, title } = project;
-                        const increment = `0${id}`;
-
-                        return (
-                            <ListItem key={id}>
-                                <Link as={`/work/${slug}`} href={{ pathname: `/work/[slug]`, query: {post: slug}}} passHref>
-                                    <LinkItem><span className="sr-only">View</span> {`${title}`}<Sup>{increment}</Sup></LinkItem>
-                                </Link>
-                            </ListItem>
-                        )
-                    })}
-                </List>
-            </Sticky>
-            <ImageGrid>
-                {projects && projects.map( project => {
-                    const { hero, slug, id } = project;
-                    const { fields } = hero;
-                    const { file, title } = fields;
-                    const { url } = file;
-                    return <ImageGridItem key={`thumbnail-${id}`}><Link as={`/work/${slug}`} href={{ pathname: `/work/[slug]`, query: {post: slug}}} passHref><ImageLink><ImageGridImage loading="lazy" src={`${url}?w=1560&h=1168`} alt={title} width="780" height="560" /></ImageLink></Link></ImageGridItem>
-                })}
-            </ImageGrid>
-        </Section>
-    )
+              return (
+                <ListItem key={id}>
+                  <Link as={`/work/${slug}`} href={{ pathname: `/work/[slug]`, query: { post: slug } }} passHref>
+                    <LinkItem>
+                      <span className="sr-only">View</span> {`${title}`}
+                      <Sup>{increment}</Sup>
+                    </LinkItem>
+                  </Link>
+                </ListItem>
+              )
+            })}
+        </List>
+      </Sticky>
+      <ImageGrid>
+        {projects &&
+          projects.map((project) => {
+            const { hero, slug, id } = project
+            const { fields } = hero
+            const { file, title } = fields
+            const { url } = file
+            return (
+              <ImageGridItem key={`thumbnail-${id}`} key={id}>
+                <Link as={`/work/${slug}`} href={{ pathname: `/work/[slug]`, query: { post: slug } }} passHref>
+                  <ImageLink>
+                    <ImageGridImage
+                      loading="lazy"
+                      src={`${url}?w=1560&h=1168&q=100`}
+                      alt={title}
+                      width="780"
+                      height="560"
+                    />
+                  </ImageLink>
+                </Link>
+              </ImageGridItem>
+            )
+          })}
+      </ImageGrid>
+    </Section>
+  )
 }
 
 export default Projects
--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -1,106 +1,140 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-import Contentful from "../../contentful/contentful";
-import Meta from "../../components/Meta/Meta";
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { toObject } from '../../util/toObject';
-import ProgressiveImage from 'react-progressive-image'
+import React from "react"
+import dynamic from "next/dynamic"
+import Contentful from "../../contentful/contentful"
+import Meta from "../../components/Meta/Meta"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { toObject } from "../../util/toObject"
+import ProgressiveImage from "react-progressive-image"
 
-import {Title, Role, Grid, GridDescription, Figure, Description, CreditTitle, DefinitionTitle, DefinitionGrid, DefinitionDescription, WorkLink, Quote, Side} from '../../components/Project/Project.styled'
+import {
+  Title,
+  Role,
+  Grid,
+  GridDescription,
+  Figure,
+  Description,
+  CreditTitle,
+  DefinitionTitle,
+  DefinitionGrid,
+  DefinitionDescription,
+  WorkLink,
+  Quote,
+  Side,
+} from "../../components/Project/Project.styled"
 
-const Layout = dynamic(() => import('../../components/Layout/Layout'));
+const Layout = dynamic(() => import("../../components/Layout/Layout"))
 
 const SingleWork = ({ work }) => {
+  const { title, slug, role, excerpt, hero, images, quote, description, workUrl, credits, creditName } = work
 
-    const { title, slug, role, excerpt, hero, images, quote, description, workUrl, credits, creditName } = work;
+  const { fields } = hero
+  const { file } = fields
+  const { url, details } = file
+  const heroAlt = fields.title
 
-    const { fields } = hero;
-    const { file } = fields;
-    const { url, details } = file;
-    const heroAlt = fields.title;
+  const creditListing = toObject(credits, creditName)
 
-    const creditListing = toObject(credits, creditName);
-
-    return (
-        <Layout>
-            <Meta
-                title={`${title} | Branding | Nicola Acquisto`}
-                description={`${title} by Nicola Acquisto`}
-                url={`https://nicolaacquisto.com/work/${slug}`}
-            />
-            <div itemScope itemType="http://schema.org/CreativeWork">
-                <Grid>
-                    <article>
-                        {title && (<Title>{title}</Title>)}
-                        {role && (<Role>Role: {role}</Role>)}
-                        {excerpt && (<Description>{documentToReactComponents(excerpt)}</Description>)}
-                    </article>
-                    <aside>
-                        <CreditTitle>Credits</CreditTitle>
-                        <DefinitionGrid>
-                            {Object.entries(creditListing).map( listing => {
-                                return (
-                                    <>
-                                        <DefinitionTitle>{listing[0]}</DefinitionTitle>
-                                        <DefinitionDescription>{listing[1]}</DefinitionDescription>
-                                    </>
-                                )
-                            })}
-                        </DefinitionGrid>
-                        {workUrl && (
-                            <WorkLink href={workUrl} target="_blank" rel="noopener noreferrer">
-                                Visit {title}
-                                <svg width="22" height="22" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M25.0023 24.4645L24.9169 9.51653M24.9169 9.51653L9.96899 9.43111M24.9169 9.51653L8.5169 25.9165" stroke="black" strokeWidth="2.5"/>
-                                </svg>
-                            </WorkLink>
-                        )}
-                    </aside>
-                </Grid>
-                <Figure>
-                    <ProgressiveImage src={`${url}?w=1687&h=1265`} placeholder="tiny-image.jpg">
-                        {(src, loading) => (
-                            <img loading="lazy" style={{ opacity: loading ? 0.5 : 1 }} src={src} alt={heroAlt} width="1687" height="1265" />
-                        )}
-                    </ProgressiveImage>
-                </Figure>
-                <GridDescription>
-                    <article>
-                        {quote && (<Quote><p>{quote}</p></Quote>)}
-                    </article>
-                    <aside>
-                        {description && <Side>{documentToReactComponents(description)}</Side>}
-                    </aside>
-                </GridDescription>
-                {images && images.map( image => {
-                    const { fields } = image;
-                    const { file, title } = fields;
-                    const { url } = file;
-                    return (
-                        <Figure>
-                            <ProgressiveImage src={`${url}?w=1687&h=1265`} placeholder="tiny-image.jpg">
-                                {(src, loading) => (
-                                    <img loading="lazy" style={{ opacity: loading ? 0.5 : 1 }} src={src} alt={title} width="1687" height="1265" />
-                                )}
-                            </ProgressiveImage>
-                        </Figure>
-                    )
-                })}
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Meta
+        title={`${title} | Branding | Nicola Acquisto`}
+        description={`${title} by Nicola Acquisto`}
+        url={`https://nicolaacquisto.com/work/${slug}`}
+      />
+      <div itemScope itemType="http://schema.org/CreativeWork">
+        <Grid>
+          <article>
+            {title && <Title>{title}</Title>}
+            {role && <Role>Role: {role}</Role>}
+            {excerpt && <Description>{documentToReactComponents(excerpt)}</Description>}
+          </article>
+          <aside>
+            <CreditTitle>Credits</CreditTitle>
+            <DefinitionGrid>
+              {Object.entries(creditListing).map((listing) => {
+                return (
+                  <>
+                    <DefinitionTitle>{listing[0]}</DefinitionTitle>
+                    <DefinitionDescription>{listing[1]}</DefinitionDescription>
+                  </>
+                )
+              })}
+            </DefinitionGrid>
+            {workUrl && (
+              <WorkLink href={workUrl} target="_blank" rel="noopener noreferrer">
+                Visit {title}
+                <svg width="22" height="22" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M25.0023 24.4645L24.9169 9.51653M24.9169 9.51653L9.96899 9.43111M24.9169 9.51653L8.5169 25.9165"
+                    stroke="black"
+                    strokeWidth="2.5"
+                  />
+                </svg>
+              </WorkLink>
+            )}
+          </aside>
+        </Grid>
+        <Figure>
+          <ProgressiveImage src={`${url}?w=2200&q=100`} placeholder="tiny-image.jpg">
+            {(src, loading) => (
+              <img
+                loading="lazy"
+                style={{ opacity: loading ? 0.5 : 1 }}
+                src={src}
+                alt={heroAlt}
+                width="1687"
+                height="1265"
+              />
+            )}
+          </ProgressiveImage>
+        </Figure>
+        <GridDescription>
+          <article>
+            {quote && (
+              <Quote>
+                <p>{quote}</p>
+              </Quote>
+            )}
+          </article>
+          <aside>{description && <Side>{documentToReactComponents(description)}</Side>}</aside>
+        </GridDescription>
+        {images &&
+          images.map((image) => {
+            const { fields } = image
+            const { file, title } = fields
+            const { url } = file
+            return (
+              <Figure>
+                <ProgressiveImage src={`${url}?w=2200&q=100`} placeholder="tiny-image.jpg">
+                  {(src, loading) => (
+                    <img
+                      loading="lazy"
+                      style={{ opacity: loading ? 0.5 : 1 }}
+                      src={src}
+                      alt={title}
+                      width="1687"
+                      height="1265"
+                    />
+                  )}
+                </ProgressiveImage>
+              </Figure>
+            )
+          })}
+      </div>
+    </Layout>
+  )
 }
 
 export async function getServerSideProps(context) {
-    const api = new Contentful();
-    const work = await api.getEntryBySlug(context.query.slug);
+  const api = new Contentful()
+  const work = await api.getEntryBySlug(context.query.slug)
 
-    return {
-        props: { work }
-    }
+  return {
+    props: { work },
+  }
 }
 
 SingleWork.propTypes = {}
 SingleWork.defaultProps = {}
 
-export default SingleWork;
+export default SingleWork
