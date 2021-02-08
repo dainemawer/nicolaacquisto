d36b15c4f5810a0c83e26d7ced6164cf2dc20033
Armand Sallé <armand.salle@gmail.com>
2021-02-08 23:58:49 +0100

    Content: about page

--- components/About/Intro.js
+++ components/About/Intro.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from "react"
 
-import {Article, Em, Image, Paragraph, Title} from './About.styled'
+import { Article, Em, Image, Paragraph, Title } from "./About.styled"
 
 const Intro = ({ isShown, setIsShown }) => {
-    return (
-        <Article>
-            <Title>I am a South African born <Em onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>creative</Em>, currently living in London. I am still on the hunt for the best porridge in the city.</Title>
-            <Paragraph>
-                I spent some time as an art director before realising I’m interested in the why behind a story and not just the how.
-            </Paragraph>
-            <Paragraph>When I am not working, typically I'll be shooting street photography, constructing the hardest 'would you rather' questions and reading futurist novels.</Paragraph>
-            <Paragraph>I enjoy working the most when I am creating thoughtful, people-centric, honest designs. I am interested in building digital products that sync human truths with a strong creative strategy.</Paragraph>
-            <Image className={isShown ? 'image-is-overlay' : ''} src="/nicola.jpg" alt="Nicola Acquisto" />
-        </Article>
-    )
+  return (
+    <Article>
+      <Title>
+        I am a South African born{" "}
+        <Em onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+          creative
+        </Em>
+        , currently living in London. I am still on the hunt for the best porridge in the city.
+      </Title>
+      <Paragraph>
+        I spent some time as an art director, before realising I’m curious in the ‘why’ behind a story, and not just the
+        ‘how’.
+      </Paragraph>
+      <Paragraph>
+        When I am not working, typically I'll be shooting street photography, constructing the hardest 'would you
+        rather' questions, and reading futurist novels.
+      </Paragraph>
+      <Paragraph>
+        I am the most excited when creating thoughtful, people-centric, honest designs. My interested is in building
+        digital products, that sync human truths, with a strong creative strategy.
+      </Paragraph>
+      <Image className={isShown ? "image-is-overlay" : ""} src="/nicola.jpg" alt="Nicola Acquisto" />
+    </Article>
+  )
 }
 
-export default Intro;
+export default Intro
